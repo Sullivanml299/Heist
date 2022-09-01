c75c023a75fbacc0747d60917f5c5815a10cd8a1
Sullivanml299 <70675302+Sullivanml299@users.noreply.github.com>
2022-09-01 17:31:57 -0400

    added starter for stealth mode and neat looking trail shader

---
+++ Assets/PlayerSkills.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using Mirror;
+
+public class PlayerSkills : MonoBehaviour
+{
+    public SkinnedMeshRenderer _renderer;
+    public Material stealthMaterial;
+    public Material[] normalMaterials;
+    public Material[] stealthMaterials;
+    public GameObject trail;
+    StarterAssetsInputs _input;
+    bool stealthActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!GetComponent<NetworkIdentity>().isLocalPlayer) {
+            this.enabled = false;
+            return;
+        }
+        normalMaterials = _renderer.materials;
+        stealthMaterials = new Material[] {stealthMaterial, stealthMaterial, stealthMaterial};
+        _input = GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_input.jump){
+            // print("TOGGLE");
+            toggleStealth();
+            _input.jump = false;
+        }
+    }
+
+    void toggleStealth(){
+        stealthActive = !stealthActive;
+        if(stealthActive) activateStealthMaterials();
+        else deactivateStealthMaterials();
+    }
+
+    void activateStealthMaterials(){
+        // print("Activate");
+        _renderer.materials= stealthMaterials;
+        trail.SetActive(true);
+    }
+
+    void deactivateStealthMaterials(){
+        // print("DEActivate");
+        _renderer.materials= normalMaterials;
+        trail.SetActive(false);
+    }
+}
